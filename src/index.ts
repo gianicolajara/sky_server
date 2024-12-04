39eb79602e30685880c749ce7324b28320baad1f
gianicola <gianicolajara@gmail.com>
2024-12-04 18:27:49 -0400

    adding HOST

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,13 @@ import { ServerProcess } from "@config/serverProcess.config";
 import app from "./app";
 import { ConsoleHelper } from "./helpers/console.helper";
 
-const server = app.listen(ServerProcess.PORT, "localhost", () => {
-  ConsoleHelper.log("🎉 App is running on PORT", ServerProcess.PORT);
-});
+const server = app.listen(
+  ServerProcess.PORT,
+  process.env.HOST || "localhost",
+  () => {
+    ConsoleHelper.log("🎉 App is running on PORT", ServerProcess.PORT);
+  }
+);
 
 export const closeServer = () => {
   if (server) {
